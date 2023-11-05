4cc5e1db41c5af1fe5437017d9f96b36f3a7758a
Carl Guo <carlguoshanghai@gmail.com>
2023-11-05 01:30:20 -0400

    ui and core web logic

--- app/database.py
+++ app/database.py
@@ -1,4 +1,31 @@
-import random
+def fetch_todo() -> list:
+    todo_list = [
+        {
+            "id": 1,
+            "task": "Do the dishes",
+            "status": "Not Started"
+        },
+        {
+            "id": 2,
+            "task": "Do the laundry",
+            "status": "Not Started"
+        },
+        {
+            "id": 3,
+            "task": "Do the chores",
+            "status": "Not Started"
+        }
+    ]
+    return todo_list
 
-def query_name():
-	return random.choice(["Alice", "Bob", "Chris", "Dolly"])
+def insert_new_task(task: str) -> None:
+    pass
+
+def update_task_entry(task_id: int, task: str) -> None:
+    pass
+
+def update_status_entry(task_id: int, status: str) -> None:
+    pass
+
+def remove_task_by_id(task_id: int) -> None:
+    pass
